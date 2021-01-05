aaf06c8fcd1aa5eca47da62fd65147c6110c6991
Alejandro Baez Corral <alejandrobaezcorral@Alejandros-MacBook-Pro.local>
2021-01-05 19:18:41 -0400

    changes_html

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask
+import requests
+from covid import Covid
+from flask import Flask, redirect, url_for, render_template
 
 
 app = Flask(__name__)
@@ -8,7 +10,18 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    url = 'http://api.ipstack.com/check?access_key=a2acd36ecdb97d72073d65b752df8eb3'
+    r = requests.get(url)
+    js = r.json()
+    strs = js['country_name']
+    covid = Covid()
+    try:
+        data = covid.get_status_by_country_name(strs)
+    except:
+        data = covid.get_status_by_country_name(js['country_code'])
+    
+    
+    return render_template("index.html", country=strs, country_code=js['country_code'], city=js['city'], cases=data["confirmed"])
 
 
 @app.route('/<name>')
